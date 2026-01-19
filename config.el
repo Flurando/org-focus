23d7782603c244a36f819adceeb6cc9e55ec6ced
R.W.Flurando <rw-flurando@inventati.org>
2026-01-19 19:25:56 +0800

    move html-head out

--- config.el
+++ config.el
@@ -1,7 +1,11 @@
 ;; this file is tweaked from the fork
 ;; you are free to customize this file anyhow you like
 
-(setq org-html-validation-link nil)
+(setq org-html-validation-link nil
+      org-html-head-include-default-style nil
+      org-html-head "\
+<link rel=\"stylesheet\" type=\"text/css\" href=\"/style.css\"/>
+")
 (setq org-publish-project-alist
       (list
        (list "org-site:main"
